3869367cff7311b096fcec66435a2d850873e620
zhaolanqi <oava126074@163.com>
2018-06-06 14:49:26 +0800

    05

---
+++ A1805/05day/02-python2不支持中文.py
@@ -0,0 +1,2 @@
+#coding=utf-8
+print('青青草原')
---
+++ A1805/05day/06-运算符练习.py
@@ -0,0 +1,19 @@
+a = 1
+b = 2
+c = a*b
+print(c)
+
+d = a+b
+print(d)
+
+e = a-b
+print(e)
+
+f = a/b
+print(f)
+
+g = a//b
+print(g)
+
+h = a**b
+print(h)
